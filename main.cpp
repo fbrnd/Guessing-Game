828715e813e4d61648a749b10ac5d8ee5fe9ccd8
Brandon <bfraz+github@protonmail.com>
2021-01-16 19:54:07 -0500

    First Commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// Three chances to guess the secret number
+
+int main()
+{
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
+
+    while(secretNum != guess && !outOfGuesses) {
+        if(guessCount < guessLimit) {
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
+
+    }
+
+    if(outOfGuesses) {
+        cout << "You Lose!";
+    } else {
+        cout << "You win!";
+    }
+
+    return 0;
+}
